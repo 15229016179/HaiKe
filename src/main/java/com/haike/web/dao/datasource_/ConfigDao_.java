1e3d383f8d8aa6aabd97812a8ae131bdd62ee139
wf_xiaoming <sgyingyin@sina.com>
2017-06-23 16:49:55 +0800

    构建spring mvc

---
+++ src/main/java/com/haike/web/dao/datasource_/ConfigDao_.java
@@ -0,0 +1,67 @@
+/*
+
+
+
+ */
+
+package com.haike.web.dao.datasource_;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.haike.web.entity.Config;
+
+/**
+ * 网站配置
+ * 
+ * @author xiaoming
+ * 
+ */
+
+@Repository("configdao_")
+public interface ConfigDao_ {
+
+	// ///////////////////////////////
+		// ///// 增加 ////////
+		// ///////////////////////////////
+
+		/**
+		 * 增加配置
+		 * 
+		 * @return Integer
+		 */
+		public int addConfig(Config config);
+
+		// ///////////////////////////////
+		// ///// 刪除 ////////
+		// ///////////////////////////////
+
+		/**
+		 * 删除配置
+		 * 
+		 * return Integer
+		 */
+		public int deleteConfig(@Param("key") String key);
+
+		// ///////////////////////////////
+		// ///// 修改 ////////
+		// ///////////////////////////////
+
+		/**
+		 * 更新配置
+		 * 
+		 * @return Integer
+		 */
+		public int updateConfig(Config config);
+
+		// ///////////////////////////////
+		// ///// 查詢 ////////
+		// ///////////////////////////////
+
+		/**
+		 * 查看配置
+		 * 
+		 * @return Config
+		 */
+		public Config getConfigByKey(@Param("key") String key);
+	}
---
+++ src/main/java/com/haike/web/service/ConfigService.java
@@ -0,0 +1,109 @@
+package com.haike.web.service;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.haike.web.dao.datasource.ConfigDao;
+import com.haike.web.dao.datasource_.ConfigDao_;
+import com.haike.web.entity.Config;
+
+/**
+ * Õ¯’æ≈‰÷√
+ * 
+ * @author xiaoming
+ * 
+ */
+@Service
+public class ConfigService {
+
+	@Resource
+	private ConfigDao configDao;
+	@Resource
+	private ConfigDao_ configDao_;
+
+	// ///////////////////////////////
+	// ///// ‘ˆº” ////////
+	// ///////////////////////////////
+
+	/**
+	 * ‘ˆº”≈‰÷√
+	 * 
+	 * @param key
+	 * @param value
+	 * @return Config
+	 */
+	public Config addConfig(String key, String value) {
+		Config config = new Config();
+		config.setKey(key);
+		config.setValue(value);
+		config.setCreateTime(new Date());
+		configDao.addConfig(config);
+		return config;
+	}
+
+	// ///////////////////////////////
+	// ///// Ñh≥˝ ////////
+	// ///////////////////////////////
+
+	/**
+	 * …æ≥˝≈‰÷√
+	 * 
+	 * @param key
+	 * @return Integer
+	 */
+	public int deleteConfigByKey(String key) {
+		return configDao.deleteConfig(key);
+	}
+
+	// ///////////////////////////////
+	// ///// –ﬁ∏ƒ ////////
+	// ///////////////////////////////
+
+	/**
+	 * ∏¸–¬≈‰÷√
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public Config updateConfigByKey(String key, String value) {
+		Config config = configDao.getConfigByKey(key);
+		config.setValue(value);
+		configDao.updateConfig(config);
+		Config config2 = configDao_.getConfigByKey(key);
+		config2.setValue(value);
+		configDao_.updateConfig(config2);
+		this.getStringByKey(key);
+		return config;
+	}
+
+	/**
+	 * @param key
+	 * @return
+	 */
+	@Cacheable(value="commonCache")
+	public String getStringByKey(String key) {
+		Config config = configDao.getConfigByKey(key);
+		if (config == null) {
+			return "";
+		} else {
+			return config.getValue();
+		}
+	}
+
+	/**
+	 * @param key
+	 * @return
+	 */
+	public int getIntKey(String key) {
+		Config config = configDao.getConfigByKey(key);
+		if (config == null) {
+			return 0;
+		} else {
+			return Integer.parseInt(config.getValue());
+		}
+	}
+}
