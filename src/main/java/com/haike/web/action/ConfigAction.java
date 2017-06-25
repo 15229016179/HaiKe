dfe5b6f3465c04a15a521bd8f9c2ec176a9e7cec
15229016179 <sgyingyin@sina.com>
2017-06-26 01:36:39 +0800

    瞎搞

---
+++ src/main/java/com/haike/web/action/ConfigAction.java
@@ -0,0 +1,38 @@
+package com.haike.web.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.haike.web.redis.template.RedisClientTemplate;
+import com.haike.web.service.ConfigService;
+
+@Controller
+@RequestMapping("/config")
+public class ConfigAction {
+	
+	@Autowired
+	ConfigService configService;
+	@Autowired
+	RedisClientTemplate redisClientTemplate;
+	
+	@RequestMapping(value = "/config.jhtml", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, String> login(HttpServletRequest request, ModelMap modelMap,
+			String requestUrlString) {
+		Map<String, String> map=new HashMap<String, String>();
+		configService.getStringByKey("dinghao_seo_headline");
+		map.put("requestUrlString", "测试");
+		redisClientTemplate.set("zihan", "nihao");
+		return map;
+	}
+
+}
---
+++ src/main/java/com/haike/web/action/MainAction.java
@@ -0,0 +1,16 @@
+package com.haike.web.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class MainAction {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "redirect:/index.html";
+	}
+
+}
