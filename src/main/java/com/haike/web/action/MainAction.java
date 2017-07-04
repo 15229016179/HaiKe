f4f50f5e10ccfbabc8c283093be18ea082d4379b
15229016179 <sgyingyin@sina.com>
2017-07-04 20:46:17 +0800

    框架完成

--- src/main/java/com/haike/web/action/MainAction.java
+++ src/main/java/com/haike/web/action/MainAction.java
@@ -1,16 +1,20 @@
 package com.haike.web.action;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class MainAction {
+	private static Logger logger = Logger.getLogger(MainAction.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
-		return "redirect:/index.html";
+	public ModelAndView home() {
+		logger.debug("MainAction home");
+		return new ModelAndView("index");
 	}
 
 }
